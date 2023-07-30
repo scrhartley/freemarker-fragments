a404c50ba4b7b927e526aa1e8aa2e7ca1a549864
scrhartley <scrhartley@github.com>
2023-07-30 17:15:41 +0100

    Initial Spring application skeleton

---
+++ src/main/java/simon/examples/freemarker/fragments/FreeMarkerFragmentsApplication.java
@@ -0,0 +1,13 @@
+package simon.examples.freemarker.fragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMarkerFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeMarkerFragmentsApplication.class, args);
+	}
+
+}
