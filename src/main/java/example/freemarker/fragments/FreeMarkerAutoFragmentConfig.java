7994261adbb0b1361e38cfed737251be709dd205
scrhartley <scrhartley@github.com>
2023-11-11 16:19:06 +0000

    Automatically make imports in main template be available to fragment

--- src/main/java/example/freemarker/fragments/FreeMarkerAutoFragmentConfig.java
+++ src/main/java/example/freemarker/fragments/FreeMarkerAutoFragmentConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.view.AbstractUrlBasedView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import freemarker.core.LibraryLoad;
 import freemarker.core.Macro;
 import freemarker.template.Template;
 
@@ -24,6 +25,8 @@ public class FreeMarkerAutoFragmentConfig {
     private static final String VIEW_FRAGMENT_SEPARATOR = " :: "; // Same as used by Thymeleaf templating
 
     private static final boolean TRANSLATE_MACRO_NAMES_ENABLED = false;
+    private static final boolean INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT = true;
+    private static final boolean FORCE_LAZY_IMPORTS_IN_FRAGMENT = true;
     private static final FragmentTemplate FRAGMENT_TEMPLATE_BUILDER = FragmentTemplate.FULLY_AUTOMATIC;
     private static final String DEFAULT_MACRO = null;
 
@@ -117,9 +120,22 @@ public class FreeMarkerAutoFragmentConfig {
         // This implementation will only execute the chosen macro, even if the base template has top level content
         // that's not in a macro. The downside of this implementation is that it uses deprecated FreeMarker methods.
         FULLY_AUTOMATIC {
-            @Override
+            @Override @SuppressWarnings("deprecation")
             Template build(String macroName, String viewName, Template baseTemplate) throws IOException {
-                Template fragmentTemplate = newTemplate("<@" + macroName + " />", viewName, baseTemplate);
+                String templateText = "<@" + macroName + " />";
+                if (INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT) {
+                    StringBuilder builder = extractImports(baseTemplate);
+                    if (builder != null) {
+                        builder.append(templateText);
+                        templateText = builder.toString();
+                    }
+                }
+                Template fragmentTemplate = newTemplate(templateText, viewName, baseTemplate);
+                if (INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT && FORCE_LAZY_IMPORTS_IN_FRAGMENT) {
+                    // If we're including the base template's imports, then it becomes
+                    // more desirable to not have to pay the cost for them unless necessarily.
+                    fragmentTemplate.setLazyImports(true);
+                }
                 @SuppressWarnings("unchecked")
                 Collection<Macro> macros = (Collection<Macro>) baseTemplate.getMacros().values();
                 macros.forEach(fragmentTemplate::addMacro);
@@ -137,6 +153,21 @@ public class FreeMarkerAutoFragmentConfig {
                     baseTemplate.getConfiguration(),
                     baseTemplate.getParserConfiguration(),
                     baseTemplate.getEncoding());
+        }
+
+        @SuppressWarnings("deprecation")
+        private static StringBuilder extractImports(Template template) {
+            @SuppressWarnings("unchecked")
+            Collection<LibraryLoad> imports = (Collection<LibraryLoad>) template.getImports();
+            if (!imports.isEmpty()) {
+                StringBuilder builder = new StringBuilder();
+                for (LibraryLoad ll : imports) {
+                    builder.append(ll.getCanonicalForm());
+                }
+                return builder;
+            } else {
+                return null;
+            }
         }
     }
 
