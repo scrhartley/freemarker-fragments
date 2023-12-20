d5cea007a064610e0824b924bf8466d98ddfb6c1
scrhartley <scrhartley@github.com>
2023-12-20 21:27:17 +0000

    Some refactoring

--- src/main/java/example/freemarker/fragments/FreeMarkerAutoFragmentConfig.java
+++ src/main/java/example/freemarker/fragments/FreeMarkerAutoFragmentConfig.java
@@ -25,9 +25,7 @@ public class FreeMarkerAutoFragmentConfig {
     private static final String VIEW_FRAGMENT_SEPARATOR = " :: "; // Same as used by Thymeleaf templating
 
     private static final boolean TRANSLATE_MACRO_NAMES_ENABLED = false;
-    private static final boolean INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT = true;
-    private static final boolean FORCE_LAZY_IMPORTS_IN_FRAGMENT = true;
-    private static final FragmentTemplate FRAGMENT_TEMPLATE_BUILDER = FragmentTemplate.FULLY_AUTOMATIC;
+    private static final FragmentTemplate FRAGMENT_TEMPLATE_BUILDER = new FragmentTemplate.FullyAutomatic();
     private static final String DEFAULT_MACRO = null;
 
     @Bean
@@ -96,7 +94,11 @@ public class FreeMarkerAutoFragmentConfig {
     }
 
 
-    enum FragmentTemplate {
+    interface FragmentTemplate {
+
+        Template build(String macroName, String viewName, Template baseTemplate) throws IOException;
+
+
         // This implementation only writes the chosen macro's output to the page,
         // but all template content is still evaluated. To use this strategy, even the top level content
         // should be a macro in order to avoid still having to provide the full template's data when using a fragment.
@@ -106,25 +108,28 @@ public class FreeMarkerAutoFragmentConfig {
         //    Perhaps this could be enhanced to verify the default macro exists in the base template
         //    when a fragment identifier isn't specified (falling back to the full template) and hence allow
         //    non-fragment templates to not be forced to have top level content be in a macro.
-        SEMI_AUTOMATIC {
+        class SemiAutomatic implements FragmentTemplate {
             @Override
-            Template build(String macroName, String viewName, Template baseTemplate) throws IOException {
+            public Template build(String macroName, String viewName, Template baseTemplate) throws IOException {
                 final String namespace = "$__auto_invoke__$";
                 String templateText =
                         "<#import \"/" + baseTemplate.getName() + "\" as " + namespace + ">" +
                         "<@" + namespace + "." + macroName + " />";
                 return newTemplate(templateText, viewName, baseTemplate);
             }
-        },
+        }
 
         // This implementation will only execute the chosen macro, even if the base template has top level content
         // that's not in a macro. One downside of this implementation is that it uses deprecated FreeMarker methods.
         // Another is that since template-level include directives are not evaluated, macros in descendant templates
         // may be unavailable to the fragment. Instead, either: prefer imports over includes, use include directives
         // inside the fragment macro or make sure the required macros are defined in the template you are calling.
-        FULLY_AUTOMATIC {
+        class FullyAutomatic implements FragmentTemplate {
+            private static final boolean INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT = true;
+            private static final boolean FORCE_LAZY_IMPORTS_IN_FRAGMENT = true;
+
             @Override @SuppressWarnings("deprecation")
-            Template build(String macroName, String viewName, Template baseTemplate) throws IOException {
+            public Template build(String macroName, String viewName, Template baseTemplate) throws IOException {
                 String templateText = "<@" + macroName + " />";
                 if (INCLUDE_TEMPLATE_IMPORTS_IN_FRAGMENT) {
                     StringBuilder builder = extractImports(baseTemplate);
@@ -146,9 +151,23 @@ public class FreeMarkerAutoFragmentConfig {
                 }
                 return fragmentTemplate;
             }
+
+            @SuppressWarnings("deprecation")
+            private static StringBuilder extractImports(Template template) {
+                @SuppressWarnings("unchecked")
+                Collection<LibraryLoad> imports = (Collection<LibraryLoad>) template.getImports();
+                if (!imports.isEmpty()) {
+                    StringBuilder builder = new StringBuilder();
+                    for (LibraryLoad ll : imports) {
+                        builder.append(ll.getCanonicalForm());
+                    }
+                    return builder;
+                } else {
+                    return null;
+                }
+            }
         };
 
-        abstract Template build(String macroName, String viewName, Template baseTemplate) throws IOException;
 
         private static Template newTemplate(
                 String templateText, String viewName, Template baseTemplate) throws IOException {
@@ -160,20 +179,6 @@ public class FreeMarkerAutoFragmentConfig {
                     baseTemplate.getEncoding());
         }
 
-        @SuppressWarnings("deprecation")
-        private static StringBuilder extractImports(Template template) {
-            @SuppressWarnings("unchecked")
-            Collection<LibraryLoad> imports = (Collection<LibraryLoad>) template.getImports();
-            if (!imports.isEmpty()) {
-                StringBuilder builder = new StringBuilder();
-                for (LibraryLoad ll : imports) {
-                    builder.append(ll.getCanonicalForm());
-                }
-                return builder;
-            } else {
-                return null;
-            }
-        }
     }
 
 }
