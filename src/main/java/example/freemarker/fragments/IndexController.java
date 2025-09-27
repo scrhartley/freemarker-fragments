3372ba9b464bce2b074bf50c04b9f3c3a8cad742
scrhartley <scrhartley@github.com>
2025-09-27 18:04:45 +0100

    Add index page

---
+++ src/main/java/example/freemarker/fragments/IndexController.java
@@ -0,0 +1,16 @@
+package example.freemarker.fragments;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String getIndexPage() {
+        return "index";
+    }
+
+}
